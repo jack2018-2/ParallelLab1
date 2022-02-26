e240fdc8f184abced6ba2944e75a5e1c7f04bdec
Evgeniy Malinovskiy <EMalinovskiy@croc.ru>
2022-02-26 15:14:11 +0300

    Добавьте файлы проекта.

---
+++ ParallelLab1/GaussBlurer.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelLab1
+{
+    public class GaussBlurer
+    {
+        List<int[,]> sourceChannels;
+        int width;
+        int height;
+        int radius;
+
+        public GaussBlurer() { }
+
+        public GaussBlurer(List<int[,]> scl, int width, int height, int radius)
+        {
+            this.sourceChannels = scl;
+            this.width = width;
+            this.height = height;
+            this.radius = radius;
+        }
+
+        public List<int[,]> Process(bool isAsync)
+        {
+            if (isAsync)
+            {
+                var safeChannels = new ConcurrentDictionary<int, int[,]>();
+                var taskBag = new Task<(int, int[,])>[3];
+                var i = 0;
+                var startTime = DateTime.Now;
+                foreach (var ch in sourceChannels)
+                {
+                    Console.WriteLine("Запущен отдельный поток обработки канала");
+                    //taskBag[i] = Task.Run(() => safeChannels.TryAdd(i, GaussBlur(ch)));
+                    taskBag[i] = Task.Run(() => (i, GaussBlur(ch)));
+                    i++;
+                }
+                Task.WaitAll(taskBag);
+                Console.WriteLine($"Обработка выполнена за {DateTime.Now - startTime}");
+                return taskBag.Select(x => x.Result).OrderBy(x => x.Item1).Select(x => x.Item2).ToList();
+                //return safeChannels.OrderBy(x => x.Key).Select(x => x.Value).ToList();
+            }
+            else
+            {
+                var channels = new List<int[,]>(); 
+                var startTime = DateTime.Now;
+                foreach (var ch in sourceChannels)
+                {
+                    Console.WriteLine("Запущена сихнронная обработка канала");
+                    channels.Add(GaussBlur(ch));
+                }
+                Console.WriteLine($"Обработка выполнена за {DateTime.Now - startTime}");
+                return channels;
+            }
+        }
+
+        private async Task<int[,]> GaussBlurAsync(int[,] sourceCh)
+        {
+            var tcl = new int[height, width];
+            var rs = Math.Ceiling(radius * 2.57);     // significant radius
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    var val = 0d;
+                    var wsum = 0d;
+                    for (var iy = i - rs; iy < i + rs + 1; iy++)
+                        for (var ix = j - rs; ix < j + rs + 1; ix++)
+                        {
+                            var x = Convert.ToInt32(Math.Round(Math.Min(width - 1, Math.Max(0, ix)), 0, MidpointRounding.AwayFromZero));
+                            var y = Convert.ToInt32(Math.Round(Math.Min(height - 1, Math.Max(0, iy)), 0, MidpointRounding.AwayFromZero));
+                            var dsq = (ix - j) * (ix - j) + (iy - i) * (iy - i);
+                            var wght = Math.Exp(-dsq / (2 * radius * radius)) / (Math.PI * 2 * radius * radius);
+                            val += sourceCh[y, x] * wght;
+                            wsum += wght;
+                        }
+                    tcl[i, j] = (int)Math.Round(val / wsum, 2);
+                }
+            }
+            return tcl;
+        }
+
+        private int[,] GaussBlur(int[,] sourceCh)
+        {
+            var tcl = new int[height, width];
+            var rs = Math.Ceiling(radius * 2.57);     // significant radius
+            for (var i = 0; i < height; i++)
+            {
+                for (var j = 0; j < width; j++)
+                {
+                    var val = 0d;
+                    var wsum = 0d;
+                    for (var iy = i - rs; iy < i + rs + 1; iy++)
+                        for (var ix = j - rs; ix < j + rs + 1; ix++)
+                        {
+                            var x = Convert.ToInt32(Math.Round(Math.Min(width - 1, Math.Max(0, ix)), 0, MidpointRounding.AwayFromZero));
+                            var y = Convert.ToInt32(Math.Round(Math.Min(height - 1, Math.Max(0, iy)), 0, MidpointRounding.AwayFromZero));
+                            var dsq = (ix - j) * (ix - j) + (iy - i) * (iy - i);
+                            var wght = Math.Exp(-dsq / (2 * radius * radius)) / (Math.PI * 2 * radius * radius);
+                            val += sourceCh[y, x] * wght;
+                            wsum += wght;
+                        }
+                    tcl[i, j] = (int)Math.Round(val / wsum, 2);
+                }
+            }
+            return tcl;
+        }
+    }
+}
---
+++ ParallelLab1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ParallelLab1
+{
+    class Program
+    {
+        static string fileName = "C:/Users/jack2/source/repos/ParallelLab1/ParallelLab1/bin/Debug/android.jpg";
+        static int radius = 3;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите полный путь к картинке. Допустимые форматы - jpeg/jpg и png.\nВ остальных случаях работоспособность не гарантируется.\n>");
+            var msg = Console.ReadLine();
+            while (!File.Exists(msg))
+            {
+                Console.Write("Картинка не найдена и/или Вы ввели неверный путь.\nВведите полный путь к картинке. Допустимые форматы - jpeg/jpg и png.\nВ остальных случаях работоспособность не гарантируется.\n>");
+                msg = Console.ReadLine();
+            }
+            fileName = msg;
+            var sourceBitmap = new Bitmap(fileName);
+            var imageChannels = ReadChannels(sourceBitmap);
+
+            var producer = new GaussBlurer(imageChannels, sourceBitmap.Width, sourceBitmap.Height, radius);
+
+            var channels1 = producer.Process(false);
+            var channels2 = producer.Process(true);
+
+            SavePic(channels1, sourceBitmap, 
+                $"{Path.GetFullPath(fileName)}_resultSync.jpg");
+            SavePic(channels2, sourceBitmap,
+                $"{Path.GetFullPath(fileName)}_resultAsync.jpg");
+        }
+
+        static List<int[,]> ReadChannels(Bitmap source)
+        {
+            int[,] r1 = new int[source.Height, source.Width];
+            int[,] g1 = new int[source.Height, source.Width];
+            int[,] b1 = new int[source.Height, source.Width];
+            for (int i = 0; i < source.Height; i++)
+            {
+                for (int j = 0; j < source.Width; j++)
+                {
+                    Color color = source.GetPixel(j, i);
+                    r1[i, j] = color.R;
+                    g1[i, j] = color.G;
+                    b1[i, j] = color.B;
+                }
+            }
+
+            return new List<int[,]>() { r1, g1, b1 };
+        }
+
+        static void SavePic(List<int[,]> channels, Bitmap source, string fileName)
+        {
+            var resBitmap = new Bitmap(source.Width, source.Height);
+            for (var i = 0; i < source.Height; i++)
+            {
+                for (var j = 0; j < source.Width; j++)
+                {
+                    resBitmap.SetPixel(j, i, Color.FromArgb(channels[0][i,j], channels[1][i, j], channels[2][i, j]));
+                }
+            }
+            resBitmap.Save(fileName);
+        }
+    }
+}
+
+/*
+ * интеграл
+ * ядро лапласа
+ * ядро собеля
+ * фильтр гаусса (фильтры изображений любые)
+ * аппроксимация ф-ции
+ * обработка строк
+ * сортировки
+ */
